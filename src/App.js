ec3abe7a941a713f670a46c7280b3362aa363967
RitvijSrivastava <ritvijsrivastava99@gmail.com>
2021-03-03 21:30:05 +0530

    :fire: Remove dependencies for MDBReact

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import SignUpContainer from './containers/SignUpContainer';
-import SignInContainer from './containers/SignInContainer';
-
+import SignUpContainer from "./containers/SignUpContainer";
+import SignInContainer from "./containers/SignInContainer";
+import Profile from "./components/Profile/Profile";
 
 function App() {
   return (
@@ -11,6 +11,7 @@ function App() {
         <Switch>
           <Route exact path="/signup" exact component={SignUpContainer} />
           <Route exact path="/signin" exact component={SignInContainer} />
+          <Route exact path="/profile" exact component={Profile} />
         </Switch>
       </Router>
     </>
--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "@fortawesome/fontawesome-free/css/all.min.css";
-import "bootstrap-css-only/css/bootstrap.min.css";
-import "mdbreact/dist/css/mdb.css";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import "bootstrap/dist/css/bootstrap.min.css";
 
 /* { REDUX } */
 import { createStore } from "redux";
