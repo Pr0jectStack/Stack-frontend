e4f466a13ca412b6b0bccb5ed3efd676ae6ecace
RitvijSrivastava <ritvijsrivastava99@gmail.com>
2021-03-06 11:17:24 +0530

    Add route for Profile

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import SignUpContainer from "./containers/SignUpContainer";
 import SignInContainer from "./containers/SignInContainer";
+import ProfileContainer from "./containers/ProfileContainer";
 import DashboardContainer from "./containers/DashboardContainer";
 import PrivateRoute from "./components/Auth/PrivateRoute";
 import GuestRoute from "./components/Auth/GuestRoute";
@@ -14,6 +15,12 @@ function App() {
           {/* Public Routes */}
           <GuestRoute exact path="/signup" exact component={SignUpContainer} />
           <GuestRoute exact path="/signin" exact component={SignInContainer} />
+          <GuestRoute
+            exact
+            path="/profile"
+            exact
+            component={ProfileContainer}
+          />
 
           {/* Private Routes */}
           <PrivateRoute
