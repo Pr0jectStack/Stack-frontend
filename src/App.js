18fbde7f74697643a7161a9b1168bcec4580705f
AkshayGupt <189301019.akshay@gmail.com>
2021-03-09 16:19:44 +0530

    :sparkles: Create new Workspace form added

--- src/App.js
+++ src/App.js
@@ -6,15 +6,29 @@ import ProfileContainer from "./containers/ProfileContainer";
 import DashboardContainer from "./containers/DashboardContainer";
 import PrivateRoute from "./components/Auth/PrivateRoute";
 import GuestRoute from "./components/Auth/GuestRoute";
+import CreateWorkspace from "./components/Workspace/CreateWorkspace";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   return (
     <>
+    <NavBar />
       <Router>
         <Switch>
           {/* Public Routes */}
           <GuestRoute exact path="/signup" exact component={SignUpContainer} />
           <GuestRoute exact path="/signin" exact component={SignInContainer} />
+          <GuestRoute exact path="/" exact component={SignInContainer} />
+         
+
+          {/* Private Routes */}
+          <PrivateRoute
+            exact
+            path="/"
+            exact
+            component={DashboardContainer}
+          />
+
           <PrivateRoute
             exact
             path="/profile"
@@ -22,12 +36,18 @@ function App() {
             component={ProfileContainer}
           />
 
-          {/* Private Routes */}
           <PrivateRoute
             exact
             path="/dashboard"
             exact
             component={DashboardContainer}
+          />
+
+           <PrivateRoute
+            exact
+            path="/createWorkspace"
+            exact
+            component={CreateWorkspace}
           />
         </Switch>
       </Router>
